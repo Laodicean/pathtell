947d5057f9ab7d2e922dedef02595f629abe47f0
Brendan Roy <br3ndanr>
2015-04-18 16:00:10 +0000

    Feed fully functional. Removed duplicate copy of bootstrap template.

--- pathtell/common/static/common/dist/js/sb-admin-2.js
+++ pathtell/common/static/common/dist/js/sb-admin-2.js
@@ -34,3 +34,15 @@ $(function() {
         element.addClass('active');
     }
 });
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing');
+    });
+});
