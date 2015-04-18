3b62a9e29ce5b8b51da12af47f226153f666d603
Brendan Roy <br3ndanr>
2015-04-18 18:47:04 +0000

    Starting login on landing

--- pathtell/landing/views.py
+++ pathtell/landing/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 
-def home(request, template='landing/home.html'):
+def home(request, template='landing/index.html'):
     context = {}
     return render(request, template, context)
