7f3a85ffbe6c3e103ebc63d57edecac465a4086b
Brendan Roy <br3ndanr>
2015-04-18 19:04:47 +0000

    getting rebase to work :/

--- pathtell/landing/views.py
+++ pathtell/landing/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 
+<<<<<<< HEAD
 def home(request, template='landing/home.html'):
     if request.user.is_authenticated():
         # return HttpResponseRedirect()
         pass
+=======
+def home(request, template='landing/index.html'):
+>>>>>>> Starting login on landing
     context = {}
     return render(request, template, context)
