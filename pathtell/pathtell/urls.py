fbee519de931472bdbdc22f899886930e9cd29e8
Tushar Bhatia <tushar@bhatia.io>
2015-04-18 16:19:11 +1000

    Added landing and feed to pathtell.urls

--- pathtell/pathtell/urls.py
+++ pathtell/pathtell/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'pathtell.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^', include('feed.urls')),
+    url(r'^', include('landing.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
