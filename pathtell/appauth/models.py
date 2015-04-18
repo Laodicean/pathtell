3f845271579bc65cb90cf2b1534188d7ff1ea834
Tushar Bhatia <tushar@bhatia.io>
2015-04-18 22:25:20 +1000

    Added is infant to model + abstract user to appauth Person

--- pathtell/appauth/models.py
+++ pathtell/appauth/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
+import datetime
 
 
-class Person(models.Model):
-    pass
+class Person(auth_models.):
+    user = models.OneToOneField()
+    dob = models.DateField()
+
+    def is_infant():
+        """ An infant is a human that is less than one year of age from now
+        """
+        if (datetime.date.today() - self.dob) <= 365:
+            return True
+        return False
