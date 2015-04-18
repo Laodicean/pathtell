23d1d212370b62267c3a6453bd0b8a58da76e568
Brendan Roy <br3ndanr>
2015-04-18 16:05:12 +0000

    Improved test context generator

--- pathtell/feed/views.py
+++ pathtell/feed/views.py
@@ -18,17 +18,17 @@ def testContext():
         }
 
     a = Alert()
-    for i in range(10):
-        a.id = i
-        a.condition = "Condition %s" % i
-        context['alerts'].append(copy.deepcopy(a))
 
     e = Event()
     for i in range(10):
         e.id = i
         e.condition = "Condition %s" % i
         e.timestamp = str(i) * 10
-        e.result = (e.id % 2 == 0)
+        e.result = (e.id % 3 == 0)
+        if e.result:
+            a.id = i
+            a.condition = "Condition %s" % i
+            context['alerts'].append(copy.deepcopy(a))
         context['events'].append(copy.deepcopy(e))
 
     return context
