a26a4993f3d746cd2f07a62bca885a6b4a919720
Tushar Bhatia <tushar@bhatia.io>
2015-04-18 22:38:05 +1000

    Updated app auth and redid migrations

--- pathtell/appauth/models.py
+++ pathtell/appauth/models.py
@@ -4,8 +4,10 @@ import datetime
 
 
 class Person(auth_models.):
-    user = models.OneToOneField()
+    email = models.EmailField(unique=True)
     dob = models.DateField()
+
+    USERNAME_FIELD = 'email'
 
     def is_infant():
         """ An infant is a human that is less than one year of age from now
--- pathtell/classifiers/views.py
+++ pathtell/classifiers/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .logic.classify import classify_for_user
 
 def classify(request):
-     
+    # request.person  
     pass
